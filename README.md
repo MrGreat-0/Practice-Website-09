b040f70c4336d10608cbd7e89dd332b548b33400
MrGreat-0 <jagjeetsinghrana2@gmail.com>
2024-05-15 23:42:37 +0530

    update
    update and fix scrolling when page loads .

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Niccolo Miranda — The Paper Portfolio
+
 # Practice-Website-09
- Simple landing page created with HTML  , CSS & JS for practice .
-It's responsive also .
+
+ Simple landing page created with HTML,CSS,JS & GSAP for practice .
+ It's mimic of Awwward Nominated Website .
+ It's responsive also .
+
+ Here website link :- https://mrgreat-0.github.io/Practice-Website-09/
--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
+    el: document.querySelector("#main"),
     smooth: true
 });
 
-var tl = gsap.timeline()
+scroll.stop();
 
-tl.to("#main", {
-    y: "100vh",
-    scale: 0.45,
-    duration: 0
-})
+var tl = gsap.timeline({
+    onComplete: function () {
+        scroll.start();
+    }
+});
 
-tl.to("#main", {
-    y: "-215vh",
-    duration: 1.5,
-    delay: 0.7
-})
+if (window.innerWidth > 1000) {
 
-tl.to("#main", {
-    y: "0vh",
-    rotate: -360,
-    scale: 1,
-    delay: 0.5,
-    duration: 1.5
-})
+    tl.to("#main", {
+        y: "100vh",
+        scale: 0.40,
+        duration: 0
+    })
+
+    tl.to("#main", {
+        y: "-262vh",
+        duration: 1.1,
+        delay: 0
+    })
+
+    tl.to("#main", {
+        y: "0vh",
+        rotate: -360,
+        scale: 1,
+        delay: 1.3,
+        duration: 1.8
+    })
+
+}
